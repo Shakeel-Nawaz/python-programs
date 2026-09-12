7c85d628bac6579f1e1c5eb2acfd46e0da50d353
Shakeel Nawaz <75359115+Shakeel-Nawaz@users.noreply.github.com>
2026-09-12 18:45:47 +0000

    Added Doc String for Merge two dictionary

--- P1-merge_two_dictionary.py
+++ P1-merge_two_dictionary.py
@@ -1,5 +1,11 @@
 """
 Merging of Dictionary, if entries found common, add the values
+
+Logic:
+1. Take Copy of 1st Dictionary
+2. Iterate Over 2nd Dictionary's Items
+3. Use Get Method with "Key of 2nd Dict and Default value 0 " and Add Value of 2nd Dict
+3. Evaluate   CopiedDict[Key from 2nd Dict]  =  CopiedDictionary.get(Key from 2nd Dict,0) + Value
 """
 
 def merge_dict(dict1,dict2):
