2bf799cf917173ad05099eb278ee5e9823599bb9
Shakeel Nawaz <Shakeelnawaz1@gmail.com>
2026-09-16 00:00:16 +0530

    P4

---
+++ P4-count_vowels_in_given_string.py
@@ -0,0 +1,20 @@
+"""
+Count Number of Vowels in Given String
+"""
+
+string = "Shakeel Nawaz"
+
+# Brute Force
+vowels = ['a','e','i','o','u','A','E','I','O','U']
+total_vowels = 0
+for char in string:
+    if char in vowels:
+        total_vowels +=1
+
+print(total_vowels)
+
+
+# using set method
+vowels = ['a','e','i','o','u','A','E','I','O','U']
+total_vowel = sum(1 for char in string if char in vowels)               # (1,1,1,1,1)
+print(total_vowel)
